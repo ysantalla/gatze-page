53047243a137ce472cef8ad29ef7783c29649208
Ing. Yasmany Santalla <ysantalla88@gmail.com>
2026-04-12 22:17:56 -0600

    feat: copy dist page

--- chunk-YYCR6PFQ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var t=[{path:"",children:[{path:"profile",loadComponent:()=>import("./chunk-G5BQVZDA.js").then(o=>o.Profile),data:{title:"Profile"}}]}];export{t as AccountRoutes};
